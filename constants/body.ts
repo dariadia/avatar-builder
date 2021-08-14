c05734da1eac6714f62c66a3692f4d6baff882a6
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-14 16:26:26 +0300

    feat(colours): split consts

---
+++ constants/body.ts
@@ -0,0 +1,17 @@
+export const LEFT = 'left'
+export const RIGHT = 'right'
+
+export const YELLOW = 'yellow'
+export const GREEN = 'green'
+export const RAINBOW = 'rainbow'
+export const ASEXUAL = 'asexual'
+export const MIST = 'mist'
+
+export const RAINBOW_COLOUR =
+  'linear-gradient(180deg, #FE0000 16.66%,#FD8C00 16.66%, 33.32%,#FFE500 33.32%, 49.98%,#119F0B 49.98%, 66.64%,#0644B3 66.64%, 83.3%,#C22EDC 83.3%)'
+export const ASEXUAL_COLOUR =
+  'linear-gradient(180deg, #181818 25%, #A3A3A3 25%, 50%, #FFFFFF 50%, 75%, #800080 75%)'
+export const NONBINARY_COLOUR =
+  'linear-gradient(180deg, #FFF430 25%, #FFFFFF 25%, 50%, #9C59D1 50%, 75%, #181818 75%)'
+export const GREYROMANTIC_COLOUR =
+  'linear-gradient(180deg, #39A33E 20%, #A3A3A3 20%, 40%, #FFFFFF 40%, 60%, #A3A3A3 60%, 80%, #39A33E 80%)'
--- src/components/BodyParts/Background.tsx
+++ src/components/BodyParts/Background.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { BACKGROUND } from '../Selector'
-
 import { mainTheme, Circle, Box, baseTheme } from 'danni-s-design-system'
+
+import {
+  GREEN,
+  YELLOW,
+  MIST,
+  RAINBOW,
+  RAINBOW_COLOUR,
+  ASEXUAL,
+  ASEXUAL_COLOUR,
+} from 'constants/body'
 
 import type { Background as BackgroundProps } from 'types'
 
@@ -16,17 +25,6 @@ export const Background = styled(Circle)<BackgroundProps>`
   overflow: hidden;
   transition: background 0.5s ease;
 `
-
-export const YELLOW = 'yellow'
-export const GREEN = 'green'
-export const RAINBOW = 'rainbow'
-export const ASEXUAL = 'asexual'
-export const MIST = 'mist'
-
-export const RAINBOW_COLOUR =
-  'linear-gradient(180deg, #FE0000 16.66%,#FD8C00 16.66%, 33.32%,#FFE500 33.32%, 49.98%,#119F0B 49.98%, 66.64%,#0644B3 66.64%, 83.3%,#C22EDC 83.3%)'
-export const ASEXUAL_COLOUR =
-  'linear-gradient(180deg, #181818 25%, #A3A3A3 25%, 50%, #FFFFFF 50%, 75%, #800080 75%)'
 
 export const BACKGROUND_OPTIONS = {
   [GREEN]: <Background colour={mainTheme.colours.accentDark} />,
