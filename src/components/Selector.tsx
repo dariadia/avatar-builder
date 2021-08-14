e789257be366b2fa861141b759f9d4fb239608c2
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-14 13:43:52 +0300

    feat(Selector): add backgrounds

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -55,6 +55,12 @@ const BACKGROUND = 'background'
 const SKIN = 'skin'
 const YELLOW = 'yellow'
 const GREEN = 'green'
+const RAINBOW = 'rainbow'
+const ASEXUAL = 'asexual'
+const RAINBOW_COLOUR =
+  'linear-gradient(180deg, #FE0000 16.66%,#FD8C00 16.66%, 33.32%,#FFE500 33.32%, 49.98%,#119F0B 49.98%, 66.64%,#0644B3 66.64%, 83.3%,#C22EDC 83.3%)'
+const ASEXUAL_COLOUR =
+  'linear-gradient(180deg, #181818 25%, #A3A3A3 25%, 50%, #FFFFFF 50%, 75%, #800080 75%)'
 
 const SELECTOR_NAMES = [BACKGROUND, SKIN]
 
@@ -63,6 +69,8 @@ type SelectorName = keyof typeof SELECTOR_NAMES
 const BACKGROUND_OPTIONS = {
   [GREEN]: <Background colour={mainTheme.colours.accentDark} />,
   [YELLOW]: <Background colour="yellow" />,
+  [RAINBOW]: <Background colour={RAINBOW_COLOUR} />,
+  [ASEXUAL]: <Background colour={ASEXUAL_COLOUR} />,
 }
 
 type BackgroundKey = keyof typeof BACKGROUND_OPTIONS
@@ -92,6 +100,20 @@ const BACKGROUNDS = [
     id: YELLOW,
     children: (
       <BackgroundWrapper>{BACKGROUND_OPTIONS[YELLOW]}</BackgroundWrapper>
+    ),
+  },
+  {
+    name: BACKGROUND,
+    id: RAINBOW,
+    children: (
+      <BackgroundWrapper>{BACKGROUND_OPTIONS[RAINBOW]}</BackgroundWrapper>
+    ),
+  },
+  {
+    name: BACKGROUND,
+    id: ASEXUAL,
+    children: (
+      <BackgroundWrapper>{BACKGROUND_OPTIONS[ASEXUAL]}</BackgroundWrapper>
     ),
   },
 ]
@@ -206,5 +228,8 @@ const StyledInput = styled('input')`
   appearance: none;
   &:not(:checked) + label {
     cursor: pointer;
+  }
+  &:checked + label {
+    filter: brightness(0.8);
   }
 `
