c0f61ee1136760d54b0306dbd0cab2479f9f66dc
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-24 21:45:13 +0300

    feat(Eyes): add new samples

--- src/components/BodyParts/Eyes.tsx
+++ src/components/BodyParts/Eyes.tsx
@@ -7,6 +7,9 @@ import {
   Box,
   baseTheme,
   SxStyleProp,
+  TriangleUp,
+  TriangleDown,
+  TriangleProps,
 } from 'danni-s-design-system'
 import { EYES } from 'constants/body'
 
@@ -88,22 +91,46 @@ const WinkingEyes: React.FC<EyesProps> = styled(Flex)<EyesProps>`
   }
 `
 
-const TriangularEyes: React.FC<EyesProps> = styled(Flex)<EyesProps>`
-  justify-content: space-between;
-  position: absolute;
-  top: ${({ top }) => (top ? top : '32px')};
-  left: -7px;
-  &::before,
-  &::after {
-    display: inline-block;
-    content: '';
-    width: 0;
-    height: 0;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-bottom: 20px solid black;
-  }
-`
+const TriangularEyes: React.FC<TriangleProps> = ({
+  left,
+  right,
+  bottom,
+  colour,
+}) => (
+  <Flex
+    sx={{
+      width: '60px',
+      justifyContent: 'space-between',
+      position: 'absolute',
+      top: '30px',
+      left: '5px',
+    }}
+  >
+    <TriangleUp {...{ left, right, bottom, colour }} />
+    <TriangleUp {...{ left, right, bottom, colour }} />
+  </Flex>
+)
+
+const TriangularDownEyes: React.FC<TriangleProps & Record<string, string>> = ({
+  left,
+  right,
+  top,
+  colour,
+  eyesPosition = '40px',
+}) => (
+  <Flex
+    sx={{
+      width: '60px',
+      justifyContent: 'space-between',
+      position: 'absolute',
+      top: eyesPosition,
+      left: '5px',
+    }}
+  >
+    <TriangleDown {...{ left, right, top, colour }} />
+    <TriangleDown {...{ left, right, top, colour }} />
+  </Flex>
+)
 
 export const Eyes = {
   OVAL: <OvalEyes />,
@@ -111,7 +138,27 @@ export const Eyes = {
   NARROW: <NarrowEyes />,
   NARROW_SAD: <NarrowEyes {...{ top: '53px' }} />,
   WINK: <WinkingEyes />,
-  TRIANGULAR: <TriangularEyes />,
+  TRIANGULAR: (
+    <TriangularEyes
+      {...{ left: '12', right: '12', bottom: '24', colour: 'black' }}
+    />
+  ),
+  TRIANGULAR_DOWN: (
+    <TriangularDownEyes
+      {...{ left: '12', right: '12', top: '24', colour: 'black' }}
+    />
+  ),
+  TRIANGULAR_SAD: (
+    <TriangularDownEyes
+      {...{
+        left: '12',
+        right: '12',
+        top: '24',
+        colour: 'black',
+        eyesPosition: '50px',
+      }}
+    />
+  ),
 }
 
 const Base: React.FC = ({ children }) => (
@@ -226,9 +273,25 @@ export const EYES_TYPES = {
   ),
   TRIANGULAR: (
     <Base>
+      <BaseLayer sx={{ top: baseTheme.space.s }}>
+        <TriangleUp left="7" right="7" bottom="15" colour="black" />
+        <TriangleUp left="7" right="7" bottom="15" colour="black" />
+      </BaseLayer>
+    </Base>
+  ),
+  TRIANGULAR_DOWN: (
+    <Base>
+      <BaseLayer sx={{ top: baseTheme.space.s }}>
+        <TriangleDown left="7" right="7" top="15" colour="black" />
+        <TriangleDown left="7" right="7" top="15" colour="black" />
+      </BaseLayer>
+    </Base>
+  ),
+  TRIANGULAR_SAD: (
+    <Base>
       <BaseLayer sx={{ top: baseTheme.space.m }}>
-        <Box width="5px" height="10px" sx={{ background: 'black' }} />
-        <Box width="10px" height="5px" sx={{ background: 'black' }} />
+        <TriangleDown left="7" right="7" top="15" colour="black" />
+        <TriangleDown left="7" right="7" top="15" colour="black" />
       </BaseLayer>
     </Base>
   ),
