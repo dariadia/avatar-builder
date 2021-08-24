865328343ae57cc399810e67e54a931e6a123e23
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-24 19:10:34 +0300

    feat(Eyes): add sample

--- src/components/BodyParts/Eyes.tsx
+++ src/components/BodyParts/Eyes.tsx
@@ -1,23 +1,89 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import { Box } from 'danni-s-design-system'
+import { Flex, Circle, Box, baseTheme } from 'danni-s-design-system'
 
-export const Eyes: React.FC = () => <Box>eyes</Box>
+import { EyesTypeKey, SelectorItem } from 'types'
+import { EYES } from 'constants/body'
 
-// const Eyes = styled(Flex)`
-//   justify-content: space-between;
-//   position: absolute;
-//   top: 12px;
-//   left: -7px;
-//   &::before,
-//   &::after {
-//     background: black;
-//     display: inline-block;
-//     content: '';
-//     width: 15px;
-//     height: 30px;
-//     margin: 0 14px;
-//     border-radius: 200px;
-//     animation: ${blink} 3s ease-in-out infinite;
-//   }
-// `
+export const Eyes: React.FC = styled(Flex)`
+  justify-content: space-between;
+  position: absolute;
+  top: 12px;
+  left: -7px;
+  &::before,
+  &::after {
+    background: black;
+    display: inline-block;
+    content: '';
+    width: 15px;
+    height: 30px;
+    margin: 0 14px;
+    border-radius: 200px;
+  }
+`
+
+const Base: React.FC = ({ children }) => (
+  <Circle
+    size={`${baseTheme.space.xxxl}px`}
+    my="m"
+    mr="m"
+    inlineBlock
+    sx={{
+      border: '1px solid grey',
+      position: 'relative',
+    }}
+  >
+    {children}
+  </Circle>
+)
+
+export const EYES_TYPES = {
+  OVAL: (
+    <Base>
+      <Flex
+        width="100%"
+        sx={{
+          justifyContent: 'space-around',
+          position: 'absolute',
+          top: baseTheme.space.s,
+        }}
+      >
+        <Box
+          width="10px"
+          height="20px"
+          sx={{ background: 'black', borderRadius: `${baseTheme.radii.l}px` }}
+        />
+        <Box
+          width="10px"
+          height="20px"
+          sx={{ background: 'black', borderRadius: `${baseTheme.radii.l}px` }}
+        />
+      </Flex>
+    </Base>
+  ),
+  ROUND: (
+    <Base>
+      <Box bg="darkest"></Box>
+    </Base>
+  ),
+  NARROW: (
+    <Base>
+      <Box bg="darkest"></Box>
+    </Base>
+  ),
+}
+
+export const EYES_ITEMS = (): SelectorItem[] => {
+  const eyesNodesArray = []
+
+  for (const eyes in EYES_TYPES) {
+    eyesNodesArray.push({
+      name: EYES,
+      id: eyes,
+      children: EYES_TYPES[eyes as EyesTypeKey],
+    })
+  }
+
+  return eyesNodesArray
+}
