f08b240838ad5c7ac6f7fdaef7f960bb448dee96
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-24 20:01:42 +0300

    feat(Eyes): add variants

--- src/components/BodyParts/Eyes.tsx
+++ src/components/BodyParts/Eyes.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Flex, Circle, Box, baseTheme } from 'danni-s-design-system'
-
-import { EyesTypeKey, SelectorItem } from 'types'
+import {
+  Flex,
+  Circle,
+  Box,
+  baseTheme,
+  SxStyleProp,
+} from 'danni-s-design-system'
 import { EYES } from 'constants/body'
 
-const OvalEyes: React.FC = styled(Flex)`
+import { EyesTypeKey, SelectorItem, Eyes as EyesProps } from 'types'
+
+const OvalEyes: React.FC<EyesProps> = styled(Flex)<EyesProps>`
   justify-content: space-between;
   position: absolute;
-  top: 32px;
+  top: ${({ top }) => (top ? top : '32px')};
   left: -7px;
   &::before,
   &::after {
@@ -23,10 +29,45 @@ const OvalEyes: React.FC = styled(Flex)`
   }
 `
 
+const RoundEyes: React.FC<EyesProps> = styled(Flex)<EyesProps>`
+  justify-content: space-between;
+  position: absolute;
+  top: ${({ top }) => (top ? top : '40px')};
+  left: -7px;
+  &::before,
+  &::after {
+    background: black;
+    display: inline-block;
+    content: '';
+    width: 15px;
+    height: 15px;
+    margin: 0 14px;
+    border-radius: 200px;
+  }
+`
+
+const NarrowEyes: React.FC<EyesProps> = styled(Flex)<EyesProps>`
+  justify-content: space-between;
+  position: absolute;
+  top: ${({ top }) => (top ? top : '42px')};
+  left: -16px;
+  &::before,
+  &::after {
+    background: black;
+    display: inline-block;
+    content: '';
+    width: 23px;
+    height: 10px;
+    margin: 0 14px;
+    border-radius: 100px;
+  }
+`
+
 export const Eyes = {
   OVAL: <OvalEyes />,
-  ROUND: <OvalEyes />,
-  NARROW: <OvalEyes />,
+  ROUND: <RoundEyes />,
+  NARROW: <NarrowEyes />,
+  NARROW_SAD: <NarrowEyes {...{ top: '53px' }} />,
 }
 
 const Base: React.FC = ({ children }) => (
@@ -44,13 +85,14 @@ const Base: React.FC = ({ children }) => (
   </Circle>
 )
 
-const BaseLayer: React.FC = ({ children }) => (
+const BaseLayer: React.FC<{ sx?: SxStyleProp }> = ({ children, sx }) => (
   <Flex
     width="100%"
     sx={{
       justifyContent: 'space-around',
       position: 'absolute',
       top: baseTheme.space.s,
+      ...sx,
     }}
   >
     {children}
@@ -93,6 +135,22 @@ export const EYES_TYPES = {
   NARROW: (
     <Base>
       <BaseLayer>
+        <Box
+          width="10px"
+          height="5px"
+          sx={{ background: 'black', borderRadius: `${baseTheme.radii.m}px` }}
+        />
+        <Box
+          width="10px"
+          height="5px"
+          sx={{ background: 'black', borderRadius: `${baseTheme.radii.m}px` }}
+        />
+      </BaseLayer>
+    </Base>
+  ),
+  NARROW_SAD: (
+    <Base>
+      <BaseLayer sx={{ top: baseTheme.space.m }}>
         <Box
           width="10px"
           height="5px"
