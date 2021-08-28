e1fcb1744cccff549a6d896a293abe0657d7b153
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-28 21:46:38 +0300

    feat(avatar): render new body part

--- src/components/BodyParts/Avatar.tsx
+++ src/components/BodyParts/Avatar.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 
-import { Skin, Background, Clothes, Eyes } from '@/components'
+import { Skin, Background, Clothes, Eyes, Eyebrows } from '@/components'
 import { Box } from 'danni-s-design-system'
 
-import { BACKGROUND_COLOURS, SKIN_COLOURS } from 'constants/body'
+import { BACKGROUND_COLOURS, HAIR_COLOURS, SKIN_COLOURS } from 'constants/body'
 
 import {
   Avatar as AvatarProps,
   BackgroundColourKey,
   SkinColourKey,
   EyesTypeKey,
+  HairColourKey,
 } from 'types'
 
 export const Avatar: React.FC<AvatarProps> = avatar => {
-  const { skin, clothes, background, eyes } = avatar
+  const { skin, clothes, background, eyes, eyebrows } = avatar
 
-  const [type, hue] = skin.split(':')
-  const colour = SKIN_COLOURS[hue as SkinColourKey]
+  const [skinType, skinHue] = skin.split(':')
+  const skinColour = SKIN_COLOURS[skinHue as SkinColourKey]
+
+  const [eyebrowsType, eyebrowsHue] = eyebrows.split(':')
+  const eyebrowsColour = HAIR_COLOURS[eyebrowsHue as HairColourKey]
 
   return (
     <Box my="m" mx="auto" width="300px" height="300px">
@@ -24,7 +28,10 @@ export const Avatar: React.FC<AvatarProps> = avatar => {
         id="avatar"
         colour={BACKGROUND_COLOURS[background as BackgroundColourKey]}
       >
-        <Skin {...{ type, colour }}> {Eyes[eyes as EyesTypeKey]}</Skin>
+        <Skin type={skinType} colour={skinColour}>
+          <Eyebrows type={eyebrowsType} colour={eyebrowsColour} />
+          {Eyes[eyes as EyesTypeKey]}
+        </Skin>
         <Clothes colour={clothes} />
       </Background>
     </Box>
