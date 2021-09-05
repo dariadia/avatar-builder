7306549b67a5d58424bb0c3ecee15b29a59a60fb
Daria Diachkova <daria.diachkova@mail.ru>
2021-09-05 19:28:34 +0400

    feat(Nose): add

--- src/components/BodyParts/Nose.tsx
+++ src/components/BodyParts/Nose.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+import { darken } from 'polished'
 
+import { NOSE } from 'constants/body'
 import { Box } from 'danni-s-design-system'
 
-export const Nose: React.FC = () => <Box>Nose</Box>
+import type { Nose as NoseProps, SelectorItem } from 'types'
 
-// const Nose = styled(Box)`
-//   width: 15px;
-//   height: 10px;
-//   position: absolute;
-//   top: 53px;
-//   left: 28px;
-//   border-radius: 40px;
-//   background: ${darken(0.06, SKIN)};
-// `
+export const NoseButton: React.FC<NoseProps> = styled(Box)<NoseProps>`
+  width: 15px;
+  height: 10px;
+  position: absolute;
+  top: 65px;
+  left: 28px;
+  border-radius: 40px;
+  background: ${({ colour }) => darken(0.06, colour)};
+`
+
+export const Nose: React.FC<NoseProps> = ({ colour, type }) => {
+  switch (type) {
+    case 'BUTTON':
+      return <NoseButton colour={colour} />
+    default:
+      return <NoseButton colour={colour} />
+  }
+}
+
+export const NOSE_ITEMS = (): SelectorItem[] => {
+  const noseNodesArray = []
+
+  noseNodesArray.push({
+    name: NOSE,
+    id: `BUTTON`,
+    children: <NoseButton colour="black" />,
+  })
+
+  return noseNodesArray
+}
