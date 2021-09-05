1365608af36099b6f824d5b70fe8bf1f49dbd7fc
Daria Diachkova <daria.diachkova@mail.ru>
2021-09-05 19:27:37 +0400

    feat(index): extend default state

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -14,6 +14,7 @@ import {
   EYEBROWS,
   CLOTHES,
   MOUTH,
+  NOSE,
 } from 'constants/body'
 
 import { ProjectLayout, Selector, Avatar } from '@/components'
@@ -62,6 +63,7 @@ const HomePage: Page<SinglePageProps> = () => {
     [EYES]: 'OVAL',
     [EYEBROWS]: 'DEFAULT:PERSIAN_SUN',
     [MOUTH]: 'SLIM:CHOCOLATE',
+    [NOSE]: 'BUTTON',
   })
 
   return (
