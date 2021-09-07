082b70e24b4e02ae13d066d174e9e4a1d6d93f5f
Daria Diachkova <daria.diachkova@mail.ru>
2021-09-07 11:25:59 +0400

    feat(Sample): add fallback

--- src/components/BodyParts/Sample.tsx
+++ src/components/BodyParts/Sample.tsx
@@ -4,8 +4,8 @@ import { baseTheme, Circle } from 'danni-s-design-system'
 import { MouthColour, EyebrowsColour, SkinColour } from 'types'
 
 export const Sample: React.FC<{
-  colour: EyebrowsColour | MouthColour | SkinColour | 'white'
-}> = ({ colour, children }) => (
+  colour?: EyebrowsColour | MouthColour | SkinColour
+}> = ({ colour = 'white', children }) => (
   <Circle
     size={`${baseTheme.space.xxxl}px`}
     my="m"
