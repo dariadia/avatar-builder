15000964bd2eeef75966c8cb71374ac161287e74
Daria Diachkova <daria.diachkova@mail.ru>
2021-09-14 19:01:56 +0300

    Revert "feat(Selector): try styles"
    This reverts commit 77baedb5bacbeb41a6cfe1a309fa0f29f3010a10.

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -440,13 +440,11 @@ const StyledInput = styled('input')`
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-  &:not(:checked) {
+  &:not(:checked) + label {
+    cursor: pointer;
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
-  }
-  &:not(:checked) + label {
-    cursor: pointer;
   }
   &:checked + label {
     filter: brightness(0.8);
